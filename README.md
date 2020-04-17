9570a9bbd2b5721b7cfb1fc6add50239c630bfb5
INTELAMD <INTELAMD@INTEL_AMD-NB-PC>
2020-04-17 22:03:36 +0700

    latihanjavareport

---
+++ README.md
@@ -0,0 +1,2 @@
+tugas java2 latihan 2
+Wahyu Riadi 2017804102 SI 6 D
---
+++ barang.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 17, 2020 at 04:34 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `java2latihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE IF NOT EXISTS `barang` (
+  `kodebarang` varchar(10) NOT NULL,
+  `namabarang` varchar(100) NOT NULL,
+  `kategori` varchar(20) NOT NULL,
+  `satuan` varchar(10) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`kodebarang`, `namabarang`, `kategori`, `satuan`, `harga`, `jumlah`) VALUES
+('001', 'Kaos', 'T-Shirt', 'lusin', 5000, 3),
+('002', 'celana', 'sport', 'pcs', 20000, 3),
+('003', 'seragam SMP', 'seragam sekolah', 'pcs', 30000, 2),
+('004', 'jersey', 'sport', 'pcs', 25000, 6);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
